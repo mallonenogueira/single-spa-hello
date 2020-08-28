d76af2bb45d6ef1bfbde4b20c44ca0546d37c194
Mallone <mallonenogueira@gmail.com>
2020-08-28 12:00:27 -0300

    change vue build

--- packages/vue-app/vue.config.js
+++ packages/vue-app/vue.config.js
@@ -1,5 +1,15 @@
 module.exports = {
+  filenameHashing: false,
   chainWebpack: (config) => {
+    config.plugins.delete("html");
+    config.plugins.delete("preload");
+    config.plugins.delete("prefetch");
+    config.entryPoints.delete("app");
     config.externals(["@mallone/state-management", "single-spa"]);
+  },
+  configureWebpack: {
+    entry: {
+      vue_app: "./src/main.js",
+    },
   },
 };
