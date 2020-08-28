40c04eda01e36403fcc31aa4407dd619da4e3bf3
mallonenogueira <mallonenogueira@gmail.com>
2020-08-28 14:49:26 +0000

    Deploying to gh-pages from  @ fc6ccea24d6ff798d7b99cd2f741c9203e5f90b8 🚀

--- packages/vue-app/src/set-public-path.js
+++
@@ -1,3 +0,0 @@
-import { setPublicPath } from "systemjs-webpack-interop";
-
-setPublicPath("@mallone/vue-app", 2);
--- packages/vue-app/vue.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  chainWebpack: (config) => {
-    config.externals(["@mallone/state-management", "single-spa"]);
-  },
-};
